9882ccc4c912ac5e7783a6de2f69499f3d70c2e0
Somesh Raj <someshraj78569@gmail.com>
2025-07-09 13:21:19 +0530

    Kahn's Algorithm (BFS For Topological Sorting)

--- 2-Cycle Detection in Graph/Toplogical Sorting/main.py
+++ 2-Cycle Detection in Graph/Toplogical Sorting/main.py
@@ -25,3 +25,43 @@ class Solution:
         while Stack:
             res.append(Stack.pop())
         return res
+    
+# Using BFS
+
+# Kahn's Algorithm
+
+class Solution:
+    
+    def topoSort(self, V, edges):
+        # Code here
+        
+        from collections import defaultdict
+        # Make a Graph
+        adj = defaultdict(list)
+        # Directed Graph hai isilie sirf ekk side hi jaa skte 1--->2
+        for u, v in edges:
+            adj[u].append(v)
+        
+        # Write the indegree of all the edges...
+        indegree = [0] * V
+        for u in range(V):
+            for v in adj[u]:
+                indegree[v] += 1
+        
+        # Take a queue and fill it by adding those edges whose indegree is currently 0
+        from collections import deque
+        queue = deque()
+        for i in range(V):
+            if indegree[i] == 0:
+                queue.append(i)
+        res = []
+        while queue:
+            q = queue.popleft()
+            res.append(q)
+            for v in adj[q]:
+                indegree[v] -= 1
+                if indegree[v] == 0:
+                    queue.append(v)
+        return res
+                    
+        
